7a42c91d00fa43b27e4fedde2d3c99634f917a4b
Jessie Mindel <jessie.mindel@gmail.com>
2022-11-03 21:45:53 -0400

    Boiler the plate

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+	<p>Hello, world!</p>,
+	document.getElementById("root")
+);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+	new CopyPlugin({
+		patterns: [
+			{ from: '**/*.html', to: './', context: 'src/' },
+		],
+	}),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
