650f3cc44877d9f0784d73a13a4bd1420f8546db
Tiffany Chen <tiffanysta.c@gmail.com>
2022-11-11 14:22:09 -0500

    disabled button

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -2,10 +2,11 @@ import * as React from "react";
 
 export interface Props {
 	text?: string;
+    disabled?: boolean;
 }
 
 export class Button extends React.Component<Props> {
 	render() {
-		return <button type="button">{this.props.text}</button>
+		return <button type="button" disabled={this.props.disabled}>{this.props.text}</button>
 	}
 }
