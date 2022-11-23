a143dcaf2c5fe276e3f1d2f0c7b18d99569ddb87
Tiffany Chen <tiffanysta.c@gmail.com>
2022-11-23 13:46:29 -0500

    quick control commit stuff

--- src/components/App/storyData.tsx
+++ src/components/App/storyData.tsx
@@ -312,181 +312,78 @@ export const CONTROL_STORY_DATA: Array<StorySection> = [
 				title: "Welcome to " + GAME_NAME,
 				instructions: "Today, you embark on a journey together. To play the game, you'll need to answer a few questions about yourself. There are no wrong answers, so try not to overthink it.",
 			} as InfoStep,
+		],
+	},
+	{
+		steps: [
 			{
 				type: StoryStepType.Info,
-				id: 'choose-player-number',
+				id: 'fast-friends-set-1-question-1-player-1',
+				title: "Take turns answering this question:",
+				instructions: "Given the choice of anyone in the world, whom would you want as a dinner guest?",
 				backgroundImage: STAR_BG,
-				title: 'Choose your character',
-				instructions: "To play the game, one of you will need to be player 1, and the other will, you guessed it, be player 2.",
+				// TODO: Choose time and word/character limits.
 			} as InfoStep,
 		],
 	},
 	{
 		steps: [
 			{
-				type: StoryStepType.WritePrompt,
-				id: 'fast-friends-set-1-question-1-player-1',
-				player: 1,
-				playerAction: 'Take the lead',
-				title: "Answer this question:",
-				instructions: "Given the choice of anyone in the world, whom would you want as a dinner guest?",
-				exampleText: "Type (as much as you want of) your response here",
-				backgroundImage: STAR_BG,
-				cardImage: PLANET_ARRIVAL_IMG,
-				// TODO: Choose time and word/character limits.
-			} as WritePromptStep,
-			{
-				type: StoryStepType.WritePrompt,
-				id: 'fast-friends-set-1-question-1-player-2',
-				player: 2,
-				playerAction: 'Take the lead',
-				title: "Answer this question:",
-				instructions: "Given the choice of anyone in the world, whom would you want as a dinner guest?",
-				exampleText: "Type (as much as you want of) your response here",
-				backgroundImage: STAR_BG,
-				cardImage: PLANET_ARRIVAL_IMG,
-				// TODO: Choose time and word/character limits.
-			} as WritePromptStep,
-		],
-	},
-	{
-		steps: [
-			{
-				type: StoryStepType.WritePrompt,
+				type: StoryStepType.Info,
 				id: 'fast-friends-set-1-question-2-player-2',
-				player: 2,
-				playerAction: 'Take the lead',
-				title: "Answer this question:",
+				title: "Take turns answering this question:",
 				instructions: "If you could wake up tomorrow having gained any one quality or ability, what would it be?",
-				exampleText: "Type (as much as you want of) your response here",
 				backgroundImage: STAR_BG,
-				cardImage: PLANET_ARRIVAL_IMG,
 				// TODO: Choose time and word/character limits.
-			} as WritePromptStep,
-			{
-				type: StoryStepType.WritePrompt,
-				id: 'fast-friends-set-1-question-2-player-1',
-				player: 1,
-				playerAction: 'Take the lead',
-				title: "Answer this question:",
-				instructions: "If you could wake up tomorrow having gained any one quality or ability, what would it be?",
-				exampleText: "Type (as much as you want of) your response here",
-				backgroundImage: STAR_BG,
-				cardImage: PLANET_ARRIVAL_IMG,
-				// TODO: Choose time and word/character limits.
-			} as WritePromptStep,
+			} as InfoStep,
 		],
 	},
 	{
 		steps: [
 			{
-				type: StoryStepType.WritePrompt,
+				type: StoryStepType.Info,
 				id: 'fast-friends-set-2-question-3-player-2',
-				player: 2,
-				playerAction: 'Take the lead',
-				title: "Answer this question:",
+				title: "Take turns answering this question:",
 				instructions: "If a crystal ball could tell you the truth about yourself, your life, the future, or anything else, what would you want to know?",
-				exampleText: "Type (as much as you want of) your response here",
 				backgroundImage: STAR_BG,
-				cardImage: PLANET_ARRIVAL_IMG,
 				// TODO: Choose time and word/character limits.
-			} as WritePromptStep,
-			{
-				type: StoryStepType.WritePrompt,
-				id: 'fast-friends-set-2-question-3-player-1',
-				player: 1,
-				playerAction: 'Take the lead',
-				title: "Answer this question:",
-				instructions: "If a crystal ball could tell you the truth about yourself, your life, the future, or anything else, what would you want to know?",
-				exampleText: "Type (as much as you want of) your response here",
-				backgroundImage: STAR_BG,
-				cardImage: PLANET_ARRIVAL_IMG,
-				// TODO: Choose time and word/character limits.
-			} as WritePromptStep,
+			} as InfoStep,
 		],
 	},
 	{
 		steps: [
 			{
-				type: StoryStepType.WritePrompt,
+				type: StoryStepType.Info,
 				id: 'fast-friends-set-2-question-4-player-1',
-				player: 1,
-				playerAction: 'Take the lead',
-				title: "Answer this question:",
-				instructions: "How close and warm is your family? Do you feel your childhood was happier than most other people's?",
-				exampleText: "Type (as much as you want of) your response here",
-				backgroundImage: STAR_BG,
-				cardImage: PLANET_ARRIVAL_IMG,
-				// TODO: Choose time and word/character limits.
-			} as WritePromptStep,
-			{
-				type: StoryStepType.WritePrompt,
-				id: 'fast-friends-set-2-question-4-player-2',
-				player: 2,
-				playerAction: 'Take the lead',
-				title: "Answer this question:",
-				exampleText: "Type (as much as you want of) your response here",
+				title: "Take turns answering this question:",
 				instructions: "How close and warm is your family? Do you feel your childhood was happier than most other people's?",
 				backgroundImage: STAR_BG,
-				cardImage: PLANET_ARRIVAL_IMG,
 				// TODO: Choose time and word/character limits.
-			} as WritePromptStep,
+			} as InfoStep,
 		],
 	},
 	{
 		steps: [
 			{
-				type: StoryStepType.WritePrompt,
+				type: StoryStepType.Info,
 				id: 'fast-friends-set-3-question-5-player-1',
-				player: 1,
-				playerAction: 'Take the lead',
-				title: "Answer this question:",
+				title: "Take turns answering this question:",
 				instructions: "If you were going to become a close friend with your partner, what would be important for them to know?",
-				exampleText: "Type (as much as you want of) your response here",
 				backgroundImage: STAR_BG,
-				cardImage: PLANET_ARRIVAL_IMG,
 				// TODO: Choose time and word/character limits.
-			} as WritePromptStep,
-			{
-				type: StoryStepType.WritePrompt,
-				id: 'fast-friends-set-3-question-5-player-2',
-				player: 2,
-				playerAction: 'Take the lead',
-				title: "Answer this question:",
-				instructions: "If you were going to become a close friend with your partner, what would be important for them to know?",
-				exampleText: "Type (as much as you want of) your response here",
-				backgroundImage: STAR_BG,
-				cardImage: PLANET_ARRIVAL_IMG,
-				// TODO: Choose time and word/character limits.
-			} as WritePromptStep,
+			} as InfoStep,
 		],
 	},
 	{
 		steps: [
 			{
-				type: StoryStepType.WritePrompt,
+				type: StoryStepType.Info,
 				id: 'fast-friends-set-3-question-6-player-2',
-				player: 2,
-				playerAction: 'Take the lead',
-				title: "Answer this question:",
+				title: "Take turns answering this question:",
 				instructions: "If you were to die this evening with no opportunity to communicate with anyone, what would you most regret not having told someone? Why haven't you told them yet?",
-				exampleText: "Type (as much as you want of) your response here",
 				backgroundImage: STAR_BG,
-				cardImage: PLANET_ARRIVAL_IMG,
 				// TODO: Choose time and word/character limits.
-			} as WritePromptStep,
-			{
-				type: StoryStepType.WritePrompt,
-				id: 'fast-friends-set-3-question-6-player-1',
-				player: 1,
-				playerAction: 'Take the lead',
-				title: "Answer this question:",
-				instructions: "If you were to die this evening with no opportunity to communicate with anyone, what would you most regret not having told someone? Why haven't you told them yet?",
-				exampleText: "Type (as much as you want of) your response here",
-				backgroundImage: STAR_BG,
-				cardImage: PLANET_ARRIVAL_IMG,
-				// TODO: Choose time and word/character limits.
-			} as WritePromptStep,
+			} as InfoStep,
 		],
 	},
 	{
